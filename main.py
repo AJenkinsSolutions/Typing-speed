483e5c3630e10681b187ff53750ff216100a2811
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-06-10 10:09:21 -0500

    Add exit button

--- main.py
+++ main.py
@@ -5,21 +5,42 @@ def main():
     class Window():
         def __init__(self):
 
+            #   Creating main root
             self.root = Tk()
             self.root.title('Root Window')
             self.root.rowconfigure(0, weight=1)
             self.root.columnconfigure(0, weight=1)
-            self.root.geometry('600x500')
+            self.root.geometry('600x500+100+100')
 
             #   Creating our pages and configure
-            self.frame_home = Frame(self.root, background='blue')
+
+            #   Home Frame
+            self.frame_home = Frame(self.root, background='white')
+
+            #Buttons
+            self.exit_button = Button(self.frame_home, text='Exit')
+            self.exit_button.grid(row=0, column=0, padx=20, pady=20)
+
+
+
+
+
+
+
+
+
+
             self.frame_main = Frame(self.root, background='grey')
+
             self.frame_score_menu = Frame(self.root, background='white')
 
             #   Position Frames
             self.position_frames(self.frame_main, self.frame_home, self.frame_score_menu)
             #   Show Frame on screen
             self.show_frame(self.frame_home)
+
+
+
 
 
 
